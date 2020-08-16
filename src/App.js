be9688e368719373e9908ae461fd19b383004ac0
libra189 <libra189@gmail.com>
2020-08-17 08:39:12 +0900

    git first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="content" className="container-fluid">
+        <div className="row">
+          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 class="h2">水田センサー</h1>
+            </div>
+          </main>
+        </div>
+
+        <h1>水田センサー</h1>
+        <div className="row">
+          <div className="col-sm">
+            <h2>水位</h2>
+          </div>
+          <div className="col-sm">
+            <h2>水温</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
